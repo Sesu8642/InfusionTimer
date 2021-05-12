46c47d7aa50c2747c01d06f66b0cf4a5f6434cd6
Sesu8642 <sesu@sesu8642.de>
2021-05-12 22:28:25 +0200

    persist data

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,27 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:free_brew/tea.dart';
 import 'package:free_brew/tea_input_form.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+const String TEAS_SAVE_KEY = "teas";
+
+void main() async {
   runApp(FreeBrew());
 }
 
-class FreeBrew extends StatelessWidget {
-  // This widget is the root of your application.
+class FreeBrew extends StatefulWidget {
+  @override
+  AppState createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<FreeBrew> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FreeBrew',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: CollectionPage(title: 'Tea Collection'),
@@ -34,15 +34,6 @@ class FreeBrew extends StatelessWidget {
 class CollectionPage extends StatefulWidget {
   CollectionPage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,6 +42,19 @@ class CollectionPage extends StatefulWidget {
 
 class _CollectionPageState extends State<CollectionPage> {
   List<Tea> _teas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((prefs) {
+      setState(() {
+        _teas = prefs
+            .getStringList(TEAS_SAVE_KEY)
+            .map((teaJson) => Tea.fromJson(jsonDecode(teaJson)))
+            .toList();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,19 +82,19 @@ class _CollectionPageState extends State<CollectionPage> {
         onPressed: () {
           showDialog(
               context: context,
-              builder: (BuildContext context) => _buildPopupDialog(context));
+              builder: (BuildContext context) => new TeaInputDialog((tea) => {
+                    setState(() {
+                      _teas.add(tea);
+                      SharedPreferences.getInstance().then((prefs) {
+                        prefs.setStringList(TEAS_SAVE_KEY,
+                            _teas.map((tea) => jsonEncode(tea)).toList());
+                      });
+                    })
+                  }));
         },
         tooltip: 'Add Tea',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  Widget _buildPopupDialog(BuildContext context) {
-    return new TeaInputDialog((tea) => {
-          setState(() {
-            _teas.add(tea);
-          })
-        });
   }
 }
--- lib/tea.dart
+++ lib/tea.dart
@@ -7,11 +7,19 @@ class Tea {
 
   Tea(this.name, this.temperature, this.gPer100Ml, this.infusions, this.notes);
 
+  Tea.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        temperature = json['temperature'],
+        gPer100Ml = json['gPer100Ml'],
+        infusions = List<Infusion>.from(
+            json['infusions'].map((i) => Infusion.fromJson(i))),
+        notes = json['notes'];
+
   Map toJson() => {
         'name': name,
         'temperature': temperature,
         'gPer100Ml': gPer100Ml,
-        'infusionsSeconds': infusions,
+        'infusions': infusions,
         'notes': notes
       };
 }
@@ -20,6 +28,8 @@ class Infusion {
   int duration;
 
   Infusion(this.duration);
+
+  Infusion.fromJson(Map<String, dynamic> json) : duration = json['duration'];
 
   Map toJson() => {'duration': duration};
 }
