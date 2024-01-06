ec8996cb96d11f8f04afb001c2391972468236a8
Sesu8642 <45859254+Sesu8642@users.noreply.github.com>
2024-01-06 23:30:45 +0100

    extend Windows build script

--- build_release_windows.bat
+++ build_release_windows.bat
@@ -1,2 +1,9 @@
-flutter build windows
+mkdir release
+
+call flutter clean
+call flutter build windows
+
+:: zip to release directory
+powershell Compress-Archive -Force "build/windows/x64/runner/Release/*" "release/enthusiast_timer_windows.zip"
+
 pause
