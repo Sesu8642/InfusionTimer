5379d12130b4d9db217819dbb46d93210f826fb7
Sesu8642 <45859254+Sesu8642@users.noreply.github.com>
2024-01-15 22:29:08 +0100

    adjust windows release zip name

--- build_release_windows.bat
+++ build_release_windows.bat
@@ -4,6 +4,6 @@ call .\flutter\bin\flutter.bat clean
 call .\flutter\bin\flutter.bat build windows
 
 :: zip to release directory
-powershell Compress-Archive -Force "build/windows/x64/runner/Release/*" "release/enthusiast_timer_windows.zip"
+powershell Compress-Archive -Force "build/windows/x64/runner/Release/*" "release/enthusiast_tea_timer_windows.zip"
 
 pause
